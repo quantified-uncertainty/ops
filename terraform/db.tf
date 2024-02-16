cc09495e81d4ab606b9ec9eb34b3459b8264e069
Vyacheslav Matyukhin <me@berekuk.ru>
2024-02-15 22:16:02 -0600

    create guesstimate db

--- terraform/db.tf
+++ terraform/db.tf
@@ -54,6 +54,21 @@ module "dev_db" {
   create    = true
 }
 
+module "guesstimate_db" {
+  source = "./database"
+
+  providers = {
+    postgresql = postgresql.quri
+  }
+
+  cluster   = digitalocean_database_cluster.quri
+  name      = "guesstimate"
+  database  = "guesstimate"
+  role      = "guesstimate_role"
+  pool_size = 5
+  create    = true
+}
+
 
 
 moved {
