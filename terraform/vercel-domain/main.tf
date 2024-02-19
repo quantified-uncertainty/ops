fbba39083c5e00fb52867e40a1ffffb0ada40354
Vyacheslav Matyukhin <me@berekuk.ru>
2024-02-19 13:32:50 -0600

    require vercel provider

--- terraform/vercel-domain/main.tf
+++ terraform/vercel-domain/main.tf
@@ -4,6 +4,11 @@ terraform {
       source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
+
+    vercel = {
+      source  = "vercel/vercel"
+      version = "~> 0.4"
+    }
   }
 }
 
