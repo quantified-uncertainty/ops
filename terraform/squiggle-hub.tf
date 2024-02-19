afaf0faa7782e49e4f2d05449b71dbdc75985aca
Vyacheslav Matyukhin <me@berekuk.ru>
2024-02-19 17:26:54 -0600

    fix txt entries

--- terraform/squiggle-hub.tf
+++ terraform/squiggle-hub.tf
@@ -106,13 +106,25 @@ resource "digitalocean_record" "hub-sendgrid" {
     "em6594" : "u34091428.wl179.sendgrid.net",
     "s1._domainkey" : "s1.domainkey.u34091428.wl179.sendgrid.net",
     "s2._domainkey" : "s2.domainkey.u34091428.wl179.sendgrid.net",
+  }
+
+  domain = local.squiggle_hub_domain
+  name   = each.key
+  type   = "CNAME"
+  ttl    = 300
+  value  = each.value
+}
+
+resource "digitalocean_record" "hub_google_verification" {
+  for_each = {
+    # Which one is correct?
     "@" : "google-site-verification=Hsd2Gnz5SHJyJYyZMbdZDXzDnYovWfwiF2cWnIBH0C0",
     "@" : "google-site-verification=msJjgrChhh6V0p1pp0c0kj4Q_RdPEWrJk4yhRcN4uE4"
   }
 
   domain = local.squiggle_hub_domain
   name   = each.key
-  type   = "CNAME"
+  type   = "TXT"
   ttl    = 300
   value  = each.value
 }
