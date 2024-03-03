a0a1b8e486631c624e590ec80de56c23e6b41e4f
Vyacheslav Matyukhin <me@berekuk.ru>
2024-03-03 16:36:22 -0600

    drop guesstimate db

--- terraform/stacks/guesstimate/backend.tf
+++ terraform/stacks/guesstimate/backend.tf
@@ -92,3 +92,19 @@ resource "digitalocean_app" "backend" {
     }
   }
 }
+
+# # Not used yet - we're still using Heroku for Guesstimate prod
+# module "db" {
+#   source = "../../modules/database"
+
+#   providers = {
+#     postgresql = postgresql.quri
+#   }
+
+#   cluster   = digitalocean_database_cluster.quri
+#   name      = "guesstimate"
+#   database  = "guesstimate"
+#   role      = "guesstimate_role"
+#   pool_size = 5
+#   create    = true
+# }
--- terraform/stacks/quri/db.tf
+++ terraform/stacks/quri/db.tf
@@ -57,19 +57,3 @@ module "dev_db" {
   pool_size = 3
   create    = true
 }
-
-# Not used yet - we're still using Heroku for Guesstimate prod
-module "guesstimate_db" {
-  source = "../../modules/database"
-
-  providers = {
-    postgresql = postgresql.quri
-  }
-
-  cluster   = digitalocean_database_cluster.quri
-  name      = "guesstimate"
-  database  = "guesstimate"
-  role      = "guesstimate_role"
-  pool_size = 5
-  create    = true
-}
