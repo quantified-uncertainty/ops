6c0cfeeb1cd62c245e2a1e5893f8ce69c6e8a808
Vyacheslav Matyukhin <me@berekuk.ru>
2024-03-14 20:27:21 -0600

    separate read and write registry secrets

--- terraform/stacks/k8s/registry.tf
+++ terraform/stacks/k8s/registry.tf
@@ -1,3 +1,15 @@
+locals {
+  # Should include the list of all namespaces that need to use the registry.
+  # Sorry; this might be inconvenient, we'll have to figure out a better solution in the future.
+  registry_credentials_namespaces = toset([
+    "guesstimate"
+  ])
+
+  registry_credentials_rw_namespaces = toset([
+    "guesstimate"
+  ])
+}
+
 # We're using the DigitalOcean Container Registry to store our Docker images.
 # So the registry is configured here and not through Argo CD.
 
@@ -10,15 +22,19 @@ resource "digitalocean_container_registry_docker_credentials" "main" {
   registry_name = digitalocean_container_registry.main.name
 }
 
+# Separate credentials for jobs that need to push images
+resource "digitalocean_container_registry_docker_credentials" "write" {
+  registry_name = digitalocean_container_registry.main.name
+  write         = true
+}
+
+# Credentials for workloads that need to _pull_ images from the registry.
 resource "kubernetes_secret" "quri_registry_credentials" {
-  for_each = toset([
-    # Should include the list of all namespaces that need to use the registry.
-    # Sorry; this might be inconvenient, we'll have to figure out a better solution in the future.
-    "guesstimate-server"
-  ])
+  for_each = toset(local.registry_credentials_namespaces)
 
   metadata {
-    name = "quri-registry"
+    name      = "quri-registry"
+    namespace = each.key
   }
 
   data = {
@@ -26,4 +42,19 @@ resource "kubernetes_secret" "quri_registry_credentials" {
   }
 
   type = "kubernetes.io/dockerconfigjson"
+}
+
+# Credentials for workloads that need to _push_ images to the registry, i.e. CI workflows.
+# Will usually be mounted to `/kaniko/.docker`.
+resource "kubernetes_secret" "quri_registry_rw_credentials" {
+  for_each = toset(local.registry_credentials_rw_namespaces)
+
+  metadata {
+    name      = "quri-registry-write"
+    namespace = each.key
+  }
+
+  data = {
+    "config.json" = digitalocean_container_registry_docker_credentials.write.docker_credentials
+  }
 }
