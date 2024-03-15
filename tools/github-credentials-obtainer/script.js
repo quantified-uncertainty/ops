52696e73e8c7fffb5fd2672598f2e84bd97ba21a
Vyacheslav Matyukhin <me@berekuk.ru>
2024-03-14 18:34:21 -0600

    fix obtainer paths

--- tools/github-credentials-obtainer/script.js
+++ tools/github-credentials-obtainer/script.js
@@ -3,6 +3,10 @@
 const { writeFileSync } = require("fs");
 const { createAppAuth } = require("@octokit/auth-app");
 
+const TOKEN_FILENAME = "/tmp/github-token";
+const DOCKER_CONFIG_FILENAME = "/tmp/docker-config.json";
+const REGISTRY = "ghcr.io";
+
 const auth = createAppAuth({
   appId: process.env.APP_ID,
   privateKey: process.env.PRIVATE_KEY,
@@ -12,12 +16,12 @@ auth({
   type: "installation",
   installationId: process.env.INSTALLATION_ID,
 }).then(({ token }) => {
-  writeFileSync("github-token", token);
+  writeFileSync(TOKEN_FILENAME, token);
   writeFileSync(
-    "docker-config.json",
+    DOCKER_CONFIG_FILENAME,
     JSON.stringify({
       auths: {
-        "ghcr.io": {
+        [REGISTRY]: {
           auth: Buffer.from(`unused:${token}`).toString("base64"),
         },
       },
