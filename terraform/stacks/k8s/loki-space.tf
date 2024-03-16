7087aa9a870f164f83f500f2875121382aa61f0b
Vyacheslav Matyukhin <me@berekuk.ru>
2024-03-16 12:08:44 -0600

    s3-bucketName

--- terraform/stacks/k8s/loki-space.tf
+++ terraform/stacks/k8s/loki-space.tf
@@ -3,6 +3,12 @@ resource "digitalocean_spaces_bucket" "loki" {
   acl  = "private"
 }
 
+resource "kubernetes_namespace" "loki" {
+  metadata {
+    name = "loki"
+  }
+}
+
 resource "kubernetes_secret" "loki" {
   metadata {
     name      = "loki-storage-secrets"
@@ -10,7 +16,8 @@ resource "kubernetes_secret" "loki" {
   }
 
   data = {
-    s3-endpoint        = digitalocean_spaces_bucket.loki.bucket_domain_name
+    s3-bucketName        = digitalocean_spaces_bucket.loki.name
+    s3-endpoint        = digitalocean_spaces_bucket.loki.endpoint
     s3-accessKeyId     = data.onepassword_item.do_spaces_api_key.username
     s3-secretAccessKey = data.onepassword_item.do_spaces_api_key.password
   }
