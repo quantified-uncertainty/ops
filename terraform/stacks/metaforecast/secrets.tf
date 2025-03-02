bf3d60efd7be43e52663e7e839d325d1ff0c508e
Vyacheslav Matyukhin <me@berekuk.ru>
2025-03-02 13:59:09 -0300

    metaforecast db secret on github

--- terraform/stacks/metaforecast/secrets.tf
+++ terraform/stacks/metaforecast/secrets.tf
@@ -27,3 +27,17 @@ data "onepassword_item" "imgur_bearer" {
   vault = "Metaforecast"
   title = "Imgur bearer"
 }
+
+# Reference to squiggle repo GitHub Production environment defined in stacks/quri/github.tf
+data "github_repository_environment" "production" {
+  environment = "Production"
+  repository  = "squiggle"
+}
+
+# Add Metaforecast database URL as a GitHub Actions secret for migrations
+resource "github_actions_environment_secret" "metaforecast_database_url_prod" {
+  repository      = "squiggle"
+  secret_name     = "METAFORECAST_DATABASE_URL"
+  environment     = data.github_repository_environment.production.environment
+  plaintext_value = module.metaforecast.db_url
+}
